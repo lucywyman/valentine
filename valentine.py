74b4222c5828661c12571c0ffc684950f86ee6cb
Ian Kronquist <iankronquist@teleport.com>
2014-07-16 21:27:30 -0700

    Add -1 to y term to hack around matplotlib
    Without this constant there are unwanted artifacts

--- valentine.py
+++ valentine.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 t = np.linspace(0, 10, 1000)
 x = 3.9*(np.sin(t)**3)
-y = (3*np.cos(t))-(1.2*np.cos(2*t))-(0.6*np.cos(3*t))-(0.2*(np.cos(4*t)))
+y = (3*np.cos(t))-(1.2*np.cos(2*t))-(0.6*np.cos(3*t))-(0.2*(np.cos(4*t))) -1
 plt.plot(x, y, 'r')
 plt.axis('equal')
 plt.fill_between(x, y, color='red')
